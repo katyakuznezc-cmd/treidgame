ed582e1aaeb5ff5662934fa46fbd3a91bd0d6024
katyakuznezc-cmd <katyakuznezc@gmail.com>
2026-01-26 14:25:03 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // <--- ДОБАВЬ ЭТУ СТРОКУ ЗДЕСЬ
+  base: './', // Относительные пути для Telegram
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
+  }
 })
